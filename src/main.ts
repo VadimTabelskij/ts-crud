94b4b2eab25eb2cbcb6f952ca0d50b22931f47f0
VadimTabelskij <bagum94@gmail.com>
2023-01-24 21:12:01 +0200

    feat: test joinCar methods

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import App from './components/app';
+// import App from './components/app';
+import CarsCollection from './helpers/cars-collection';
+import cars from './data/cars';
+import brands from './data/brands';
+import models from './data/models';
 
-const app = new App('#root');
-app.initialize();
+const carsCollection = new CarsCollection({
+  cars,
+  brands,
+  models,
+});
+
+console.log(carsCollection);
+//  Viena apjungta mašina
+console.log(carsCollection.joinCar(cars[0]));
+//  Gauti visas mašinas
+console.log(carsCollection.all);
+
+// const app = new App('#root');
+// app.initialize();
