335a5195bc4d170a1738208cb24e16e6a0232791
VadimTabelskij <bagum94@gmail.com>
2023-01-24 22:04:30 +0200

    feat: created app component

--- src/components/app.ts
+++ src/components/app.ts
@@ -1,20 +1,28 @@
+import CarsCollection from '../helpers/cars-collection';
+import cars from '../data/cars';
+import models from '../data/models';
+import brands from '../data/brands';
+
 class App {
   private htmlElement: HTMLElement;
+
+  private carsCollection: CarsCollection;
 
   constructor(selector: string) {
     const foundElement = document.querySelector<HTMLElement>(selector);
 
-    if (foundElement === null) throw new Error(`Nerastas elementas su selektoriumi '${selector}'`);
+    if (foundElement === null) { throw new Error(`Nerastas elementas su selektoriumi '${selector}'`); }
+    if (!(foundElement instanceof HTMLElement)) {
+      throw new Error('Turi egzistuoti HTML elementas');
+    }
 
     this.htmlElement = foundElement;
+    this.carsCollection = new CarsCollection({ cars, models, brands });
+    console.log(this.carsCollection);
   }
 
   initialize = (): void => {
-    const container = document.createElement('div');
-    container.className = 'container my-5';
-    container.innerHTML = 'Laukiu kol būsiu sukurtas';
-
-    this.htmlElement.append(container);
+    this.htmlElement.innerHTML = 'Laukiu kol būsiu sukurtas';
   };
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,13 @@
-// import App from './components/app';
-import CarsCollection from './helpers/cars-collection';
+import App from './components/app';
 import cars from './data/cars';
-import brands from './data/brands';
 import models from './data/models';
+import brands from './data/brands';
 
-const carsCollection = new CarsCollection({
-  cars,
-  brands,
-  models,
-});
+import CarsCollection from './helpers/cars-collection';
+
+const app = new App('#root');
+app.initialize();
+
+const carsCollection = new CarsCollection({ cars, brands, models });
 
 console.log(carsCollection);
-//  Viena apjungta mašina
-console.log(carsCollection.joinCar(cars[0]));
-//  Gauti visas mašinas
-console.log(carsCollection.all);
-
-// const app = new App('#root');
-// app.initialize();
