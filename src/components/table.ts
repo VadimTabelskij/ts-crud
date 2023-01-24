bde508719c6a7e279a1a88f73564547517017782
VadimTabelskij <bagum94@gmail.com>
2023-01-24 23:38:49 +0200

    feat: check column and row are the same size

--- src/components/table.ts
+++ src/components/table.ts
@@ -1,15 +1,17 @@
-type RowData = {
-  id: string;
-  [key: string]: string;
+import countObjectProperties from '../helpers/count-object-properties';
+
+type TablesRowData = {
+  id: string,
+  [key: string]: string,
 };
 
- type TableProps<Type> = {
-   title: string;
-   columns: Type;
-   rowsData: Type[];
- };
+export type TableProps<Type> = {
+  title: string,
+  columns: Type,
+  rowsData: Type[],
+};
 
-class Table<Type extends RowData> {
+class Table<Type extends TablesRowData> {
   private props: TableProps<Type>;
 
   private tbody: HTMLTableSectionElement;
@@ -20,12 +22,30 @@ class Table<Type extends RowData> {
 
   public constructor(props: TableProps<Type>) {
     this.props = props;
+    this.checkColumns();
+
     this.htmlElement = document.createElement('table');
     this.thead = document.createElement('thead');
     this.tbody = document.createElement('tbody');
 
     this.initialize();
   }
+
+  private checkColumns = (): void => {
+    const { rowsData, columns } = this.props;
+
+    if (this.props.rowsData.length === 0) return;
+    const columnCount = countObjectProperties(columns);
+
+    const columnsWithRowsData = rowsData.every((row) => {
+      const rowCount = countObjectProperties(row);
+
+      return rowCount === columnCount;
+    });
+    if (!columnsWithRowsData) {
+      throw new Error('Nesutampa lentelės stulpelių skaičius su eilučių stulpelių skaičiumi');
+    }
+  };
 }
 
 export default Table;
